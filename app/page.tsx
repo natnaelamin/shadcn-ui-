e72d6bd97375d31c9f6b8576300247c1d4fdd1d6
Natnael Amin <natnaelamin24@gmail.com>
2024-09-23 23:35:03 +0300

    back working on this project after a while. gonna make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Middle from "@/components/middle";
 import Right from "@/components/right";
 
 
-
 export default function Home() {
   return (
     <div className="mt-5 px-10 py-5 rounded-xl border-2 grid grid-cols-3 gap-5"> 
